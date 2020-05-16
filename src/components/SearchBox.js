e8856587536a2877c9a9c8db79aeb0e519fbc7f4
davemac83 <davemcb36@gmail.com>
2020-05-16 18:00:06 +0100

    new bg colour

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -4,9 +4,9 @@ const SearchBox = ({ searchField, searchChange }) => {
 	return (
 	<div className='pa2'>
        <input
-        className='pa3 ba b--green bg-lightest-blue'
+        className='pa3 ba b--blue bg-lightest-blue'
         type='search'
-        placeholder='search robots'
+        placeholder='Find Robots'
         onChange={searchChange}
        />
     </div>
